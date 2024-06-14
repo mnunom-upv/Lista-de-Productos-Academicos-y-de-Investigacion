32d31339e46f08f5664e453285df7dcb3b23c700
Live session user <mnunom@upv.edu.mx>
2024-06-14 14:51:12 +0000

    Version1718376669

--- Z.sh
+++ Z.sh
@@ -1,5 +1,29 @@
+# Pasos Previos
+#git init
+#git add .
+#git commit -m "Initial Commit"
+#git config --global credential.helper store
+#git branch -M main
+#git remote add origin https://github.com/mnunom-upv/Lista-de-Productos-Academicos-y-de-Investigacion.git
+
+current=$(date '+%s')
+#echo $current
+
+#exit()
+
 pdflatex Publications.tex
 biber Publications
 pdflatex Publications.tex
 pdflatex Publications.tex
+
+# Agrega un archivo
+git add Publications.tex
+git add Z.sh
+git add Publications.bib
+
+# Establece una version
+git commit -m "Version${current}"
+# Hace el Push
+git push -u origin main
+
 atril Publications.pdf
